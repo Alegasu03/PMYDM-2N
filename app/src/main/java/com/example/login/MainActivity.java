e5f8b4228f0cc158e7d9666d361a05d921b12cdc
AlejandroGallegoS <alegasu03@gmail.com>
2023-10-05 13:03:05 +0200

    ee

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText user = findViewById(R.id.usuario);
+        TextView resulText= findViewById(R.id.result_check);
+        EditText passwd= findViewById(R.id.contraseña);
+        Button button= findViewById(R.id.boton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuariocorrecto = "galle";
+                String contraseñacorrecta = "12345";
+                String usuario = user.getText().toString();
+                String contraseña = passwd.getText().toString();
+
+                if (usuario.equals(usuariocorrecto) && contraseña.equals(contraseñacorrecta)) {
+                    resulText.setText("Usuario y contraseña correctos");
+                } else if (usuario.isEmpty() && contraseña.isEmpty()) {
+                    resulText.setText("El usuario y la contraseña no pueden estar vacíos");
+                } else if (!usuario.equals(usuariocorrecto) && contraseña.equals(contraseñacorrecta)) {
+                    resulText.setText("El usuario es incorrecto");
+                } else if (usuario.equals(usuariocorrecto) && !contraseña.equals(contraseñacorrecta)) {
+                    resulText.setText("La contraseña es incorrecta");
+                } else if (!usuario.equals(usuariocorrecto) && !contraseña.equals(contraseñacorrecta)) {
+                    resulText.setText("El usuario y la contraseña no son correctos");
+                }
+
+
+            }
+        });
+    }
+}
+
